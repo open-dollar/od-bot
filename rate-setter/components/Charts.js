e53873f90ae76918225ee70a2b01ebf6e8e04974
alekseevaiana <alekseevadev@gmail.com>
2023-08-23 14:23:49 -0700

    add function to get chart data

--- rate-setter/components/Charts.js
+++ rate-setter/components/Charts.js
@@ -1,5 +1,6 @@
 import { useQuery, gql } from "@apollo/client";
 import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 
 const GET_STATS = gql`
   query {
@@ -35,9 +36,10 @@ const GET_STATS = gql`
 const getLabels = (data) => {
   return data.map((stats) => {
     const date = new Date(Number(stats.blockTimestamp) * 1000);
-    return `${date.getUTCFullYear()}-${
-      date.getUTCMonth() + 1
-    }-${date.getUTCDate()} ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`;
+
+    return `${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
   });
 };
 
@@ -55,21 +57,55 @@ const getValues = (data, key) => {
   });
 };
 
+const getChartData = (data, labelName, key) => {
+  const labels = getLabels(data);
+  const values = getValues(data, key);
+
+  return {
+    labels: labels,
+    datasets: [
+      {
+        label: labelName,
+        data: values,
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#f0331a",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  };
+};
+
 const Charts = () => {
   const { loading, error, data } = useQuery(GET_STATS);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
   console.log(data.globalStats);
 
-  const erc20SupplyValues = getValues(data.globalStats, "erc20Supply");
-  const labels = getLabels(data.globalStats);
-  console.log("erc20SupplyValues: ", erc20SupplyValues)
-  console.log("labels: ", labels)
+  const erc20SupplyData = getChartData(
+    data.globalStats,
+    "erc20Supply",
+    "erc20Supply"
+  );
+  const marketPriceData = getChartData(
+    data.globalStats,
+    "Market Price Data",
+    "marketPrice"
+  );
 
   if (data) {
     return (
       <>
         <h1>Line Charts</h1>
+        <div style={{ width: "100%" }}>
+          <Line data={erc20SupplyData} />
+          <Line data={marketPriceData} />
+        </div>
       </>
     );
   }
