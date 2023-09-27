9b3ee09591a1adc2d11164233c88f12a42ff2f34
Patrick Gallagher <blockchainbuddha@gmail.com>
2023-09-26 18:54:57 -0700

    Add goerli eth airdrop to claim

--- pages/api/auction.js
+++ pages/api/auction.js
@@ -3,14 +3,19 @@ import { getAuctionData } from "../../lib/web3/auction";
 const ARBITRUM_GOERLI = "ARBITRUM_GOERLI";
 
 export default async function handler(request, response) {
-  if (request.query.secret !== process.env.RATE_SECRET) {
-    response.status(401).end();
-    return;
+  try {
+    if (request.query.secret !== process.env.RATE_SECRET) {
+      response.status(401).end();
+      return;
+    }
+    let network = ARBITRUM_GOERLI;
+    if (request.query.network) network = request.query.network;
+
+    await getAuctionData(network);
+
+    response.status(200).json({ success: true });
+  } catch (e) {
+    console.log(e);
+    return response.status(500).json({ success: false, error: e.message });
   }
-  let network = ARBITRUM_GOERLI;
-  if (request.query.network) network = request.query.network;
-
-  await getAuctionData(network);
-
-  response.status(200).json({ success: true });
 }
--- pages/api/rate.js
+++ pages/api/rate.js
@@ -3,14 +3,19 @@ import { updateRate } from "../../lib/web3/rate";
 const ARBITRUM_GOERLI = "ARBITRUM_GOERLI";
 
 export default async function handler(request, response) {
-  if (request.query.secret !== process.env.RATE_SECRET) {
-    response.status(401).end();
-    return;
+  try {
+    if (request.query.secret !== process.env.RATE_SECRET) {
+      response.status(401).end();
+      return;
+    }
+
+    let network = ARBITRUM_GOERLI;
+    if (request.query.network) network = request.query.network;
+    await updateRate(network);
+
+    response.status(200).json({ success: true });
+  } catch (e) {
+    console.log(e);
+    return response.status(500).json({ success: false, error: e.message });
   }
-
-  let network = ARBITRUM_GOERLI;
-  if (request.query.network) network = request.query.network;
-  await updateRate(network);
-
-  response.status(200).json({ success: true });
 }
