6511345cd3982d599b8dcf6d96ea6110137e6a7a
Patrick Gallagher <blockchainbuddha@gmail.com>
2023-11-16 16:37:58 +0300

    update sdk to v1.5.8-rc.4

--- test/index.test.js
+++ test/index.test.js
@@ -10,7 +10,7 @@ const test = async () => {
     //   `http://localhost:3000/api/rate?secret=${process.env.RATE_SECRET}`
     // );
 
-    // response = await fetch(
+    // response = await fetch
     //   `http://localhost:3000/api/analytics?secret=${process.env.RATE_SECRET}`
     // );
 
@@ -18,9 +18,9 @@ const test = async () => {
     //   `http://localhost:3000/api/oracle?secret=${process.env.RATE_SECRET}`
     // );
 
-    // response = await fetch(
-    //   `http://localhost:3000/api/auction?secret=${process.env.RATE_SECRET}`
-    // );
+    response = await fetch(
+      `http://localhost:3000/api/auction?secret=${process.env.RATE_SECRET}`
+    );
 
     // response = await fetch(
     //   `http://localhost:3000/api/user?address=0xC295763Eed507d4A0f8B77241c03dd3354781a15&secret=${process.env.RATE_SECRET}`
@@ -30,9 +30,9 @@ const test = async () => {
     //   `http://localhost:3000/api/claim?address=0xC295763Eed507d4A0f8B77241c03dd3354781a15&secret=${process.env.RATE_SECRET}`
     // );
 
-    response = await fetch(
-      `http://localhost:3000/api/price?secret=${process.env.RATE_SECRET}&token=MAGIC&price=100000000000000&execute=false`
-    );
+    // response = await fetch(
+    //   `http://localhost:3000/api/price?secret=${process.env.RATE_SECRET}&token=MAGIC&price=100000000000000&execute=false`
+    // );
 
     // response = await fetch(
     //   `http://localhost:3000/api/liquidate?secret=${process.env.RATE_SECRET}&id=1`
