d473e68f43e79e43a896ee15f45cc8b6c478bd27
jahabeebs <jahgoreds@gmail.com>
2024-03-28 18:14:39 -0400

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export function middleware(request) {
   const authHeader = request.headers.get("authorization");
 
@@ -10,6 +12,44 @@ export function middleware(request) {
       status: 401,
     });
   }
+
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+      .replace(/\s{2,}/g, ' ')
+      .trim()
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-nonce', nonce)
+
+  requestHeaders.set(
+      'Content-Security-Policy',
+      contentSecurityPolicyHeaderValue
+  )
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+  response.headers.set(
+      'Content-Security-Policy',
+      contentSecurityPolicyHeaderValue
+  )
+
+  return response
 }
 
 export const config = {
