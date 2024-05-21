aa3c355e53c1d6261ffa930e9a3bc4b2a147c6b2
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-05-20 17:09:45 -0700

    fuul api working

--- queries/fuul.js
+++ queries/fuul.js
@@ -1,39 +1,42 @@
 const dotenv = require('dotenv').config()
 
-const getQuery = (url, payload, headers) => {
+const getQuery = (url, body, headers) => {
     return fetch(url, {
         method: 'POST',
-        body: JSON.stringify({ payload }),
+        body: JSON.stringify(body),
         headers,
     })
-        .then(r => r.json())
         .then(data => {
             return data;
         })
 }
 
 const headers = {
-    'AUTHORIZATION': `Bearer ${process.env.FUUL_API_KEY}`,
+    'Authorization': `Bearer ${process.env.FUUL_API_KEY}`,
     "content-type": "application/json",
 };
 
 url = "https://api.fuul.xyz/api/v1/events"
 
-const payload = {
-    "args": {
-        "value": {
-            "amount": "100",
-            "currency": "POINTS"
+user_address = "0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5"
+
+const body = {
+    args: {
+        points: {
+            amount: 100,
+            currency: "POINTS",
         },
     },
-    "name": "galxe",
-    "user_address": "0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5",
-    "timestamp": new Date().getTime()
+    name: "socialPoints",
+    user_address: user_address,
+    timestamp: new Date().getTime(),
+    dedup_id: `${user_address}-${new Date().getTime()}`
 }
 
-getQuery(url, payload, headers)
+getQuery(url, body, headers)
     .then(data => {
-        console.log(data)
+        console.log(data.status)
+        console.log(data.statusText)
     })
     .catch(error => {
         console.error(error);
