fad109acced05b99592aba412bde0cc75f665196
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-05-23 20:38:49 -0700

    add back apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ApolloProvider } from "@apollo/client";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { Suspense } from "react";
-// import { client } from "../lib/apollo-client";
+import { client } from "../lib/apollo-client";
 import { NextUIProvider } from "@nextui-org/react";
 
 import Home from "./Home";
@@ -10,15 +10,15 @@ import "../styles/globals.css";
 
 const App = () => {
   return (
-    // <ApolloProvider client={client}>
-    <Suspense fallback={null}>
-      <NextUIProvider>
-        <main className="bg-[#f2f8fd] text-foreground w-full">
-          <Home />
-        </main>
-      </NextUIProvider>
-    </Suspense>
-    // </ApolloProvider>
+    <ApolloProvider client={client}>
+      <Suspense fallback={null}>
+        <NextUIProvider>
+          <main className="bg-[#f2f8fd] text-foreground w-full">
+            <Home />
+          </main>
+        </NextUIProvider>
+      </Suspense>
+    </ApolloProvider>
   );
 };
 
