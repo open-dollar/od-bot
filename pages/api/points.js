8e439cab629658624976f28c524f7f24677b2b74
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-05-24 22:24:27 -0700

    add camelot

--- pages/api/points.js
+++ pages/api/points.js
@@ -1,13 +1,18 @@
 import { updateGalxePointsForAllUsers } from "../../lib/galxe";
 import { updateZealyPointsForAllUsers } from "../../lib/zealy";
+import { updateCamelotPointsForAllUsers } from "../../lib/camelot"
 
 export default async function handler(request, response) {
     try {
-        const galxeData = await updateGalxePointsForAllUsers()
+        // const galxeData = await updateGalxePointsForAllUsers()
 
-        const zealyData = await updateZealyPointsForAllUsers()
+        // const zealyData = await updateZealyPointsForAllUsers()
+        const zealyData = {}
+        const galxeData = {}
+        // TODO: move to another endpoint for daily updates
+        const camelotData = await updateCamelotPointsForAllUsers()
 
-        response.status(200).json({ success: true, zealyData, galxeData });
+        response.status(200).json({ success: true, zealyData, galxeData, camelotData });
     } catch (e) {
         console.log(e);
         return response.status(500).json({ error: e.message });
