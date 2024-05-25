2479c642350c30f61a38fb6828245ffc9b9e453c
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-05-25 10:58:23 -0700

    Send production social points - round 2

--- pages/api/points.js
+++ pages/api/points.js
@@ -1,18 +1,13 @@
 import { updateGalxePointsForAllUsers } from "../../lib/galxe";
 import { updateZealyPointsForAllUsers } from "../../lib/zealy";
-import { updateCamelotPointsForAllUsers } from "../../lib/camelot"
 
 export default async function handler(request, response) {
     try {
-        // const galxeData = await updateGalxePointsForAllUsers()
+        const galxeData = await updateGalxePointsForAllUsers()
 
-        // const zealyData = await updateZealyPointsForAllUsers()
-        const zealyData = {}
-        const galxeData = {}
-        // TODO: move to another endpoint for daily updates
-        const camelotData = await updateCamelotPointsForAllUsers()
+        const zealyData = await updateZealyPointsForAllUsers()
 
-        response.status(200).json({ success: true, zealyData, galxeData, camelotData });
+        response.status(200).json({ success: true, zealyData, galxeData });
     } catch (e) {
         console.log(e);
         return response.status(500).json({ error: e.message });
