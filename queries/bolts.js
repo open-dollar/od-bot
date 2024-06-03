af87cbf1e7e34980c6617d63560b494ccef00184
jahabeebs <jahgoreds@gmail.com>
2024-06-03 18:10:18 -0500

    changed but not working

--- queries/bolts.js
+++ queries/bolts.js
@@ -1,34 +1,38 @@
+require('dotenv').config()
+
+const FUUL_API_KEY = process.env.FUUL_API_KEY
+
 const getUserBolts = async (address) => {
-    const lowercaseAddress = address.toLowerCase()
-    const url = `https://bot.opendollar.com/api/bolts?address=${lowercaseAddress}`
+    const lowercaseAddress = address.toLowerCase();
+    const url = `https://api.fuul.xyz/api/v1/payouts?type=point&user_address=${lowercaseAddress}`;
+    const HEADERS = {
+        'Authorization': `Bearer ${FUUL_API_KEY}`,
+        'Content-Type': 'application/json',
+    };
 
     try {
         const response = await fetch(url, {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        const data = await response.json()
+            headers: HEADERS
+        });
+        const data = await response.json();
+        console.log(data, 'data')
 
-        if (response.ok && data.success) {
-            const user = data.data.fuul.user
-            const bolts = user && user.points ? user.points : 0
-            return bolts
+        if (response.ok && data.results) {
+            return data.results.reduce((total, item) => total + Number(item.total_amount), 0);
         } else {
-            return 0
+            return 0;
         }
     } catch (error) {
-        console.error('Error fetching bolts:', error)
-        return 0
+        console.error('Error fetching bolts:', error);
+        return 0;
     }
-}
+};
 
-// Enter the user's address below
 getUserBolts('USER_ADDRESS_HERE')
     .then(bolts => {
-        console.log(`${bolts}`)
+        console.log(`${bolts}`);
     })
     .catch(error => {
-        console.error(error)
-    })
+        console.error(error);
+    });
