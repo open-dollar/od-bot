ef0e47835bd92fb2f15ee749d68691796f3b6c29
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-06-21 22:02:44 -0700

    re-organize into campaign types

--- pages/api/points.js
+++ pages/api/points.js
@@ -1,8 +1,13 @@
-import { giveBoltsForGaxleZealy } from "../../lib/bolts"
+import { updateBoltsByCampaign, POINTS_OPERATION } from "../../lib/bolts/bolts"
 
 export default async function handler(request, response) {
     try {
-        const data = await giveBoltsForGaxleZealy()
+        let type
+        if (request.query.type) type = request.query.type;
+        if (!type) return response.status(400).json({ error: 'Missing type' });
+        if (POINTS_OPERATION.indexOf(type) === -1) return response.status(400).json({ error: `Invalid type. Available types are ${POINTS_OPERATION.entries}` })
+
+        const data = await updateBoltsByCampaign(type)
 
         response.status(200).json({ success: true, data });
     } catch (e) {
