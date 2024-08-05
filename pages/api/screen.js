b3b5a7d07b6fb2252319a5df4c722ecdc9b5e2e5
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-08-05 13:05:38 -0700

    New collateral: Pendle and Arb-Griff (#130)
    * Issue #656 (bot PR): country.is failure (#128)
    
    * test geoblock
    
    * fix geoblock logic
    
    * disable cors temporarily
    
    * enable cors again
    
    * no co
    
    * test code with co and no cors
    
    * more testing
    
    * get country in other way
    
    * another way to get headers
    
    * try another way
    
    * fix headers again
    
    * add back cors
    
    * no vercel functions
    
    * bump sdk to v1.7.6
    
    * Add pendle (#129)
    
    * add pendle
    
    * yarn link
    
    ---------
    
    Co-authored-by: Jacob Habib <47253537+jahabeebs@users.noreply.github.com>

--- pages/api/screen.js
+++ pages/api/screen.js
@@ -3,17 +3,32 @@ import { setCorsHeaders } from "../../lib/cors";
 
 export default async function handler(request, response) {
   setCorsHeaders(request, response);
+
   try {
+    const country = request.headers['x-vercel-ip-country'];
+    const bannedCountryCodes = ['US', 'IR', 'KP'];
+
+    if (country && bannedCountryCodes.includes(country)) {
+      return response.status(200).json({ success: false, message: 'geoblocked' });
+    }
+
     const result = await fetch(
-      `https://public.chainalysis.com/api/v1/address/${request.query.address}`,
-      {
-        headers: {
-          "X-API-Key": process.env.CHAINALYSIS_KEY,
-          Accept: "application/json",
-        },
-      }
+        `https://public.chainalysis.com/api/v1/address/${request.query.address}`,
+        {
+          headers: {
+            "X-API-Key": process.env.CHAINALYSIS_KEY,
+            Accept: "application/json",
+          },
+        }
     );
-    return response.send(await result.json());
+
+    const data = await result.json();
+
+    if (!result.ok) {
+      throw new Error(data.message || 'Failed to fetch data');
+    }
+
+    return response.send(data);
   } catch (e) {
     console.log(e);
     return response.status(500).json({ success: false, error: e.message });
